60639a1ecfaeb36f8e3924a6fd023a80193bee4a
duartedouglas@icloud.com <duartedouglas@icloud.com>
2019-06-21 14:04:21 -0300

    Excluido arquivos de cache

--- nucleo/filtro/FiltroAbstrato.php
+++ nucleo/filtro/FiltroAbstrato.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace br\univali\sisnet\mvc\nucleo\filtro;
 
 use br\univali\sisnet\mvc\nucleo\Requisicao;
@@ -9,12 +8,15 @@ abstract class FiltroAbstrato
 {
     private $proximo;
 
-    public function definirProximo(FiltroAbstrato $proximo){
+    public function definirProximo(FiltroAbstrato $proximo)
+    {
         $this->proximo = $proximo;
     }
 
     public abstract function filtrar(Requisicao $requisicao);
-    public function obterProximo(){
+
+    public function obterProximo()
+    {
         return $this->proximo;
     }
 }
